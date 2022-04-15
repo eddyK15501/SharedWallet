abe408e1171ff388444367492cc6b730a6bec527
Edward Kim <edwardkim@Edwards-MacBook-Pro.local>
2022-04-15 14:57:48 -0400

    first commit

---
+++ contracts/SharedWallet.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.11;
+
+contract Allowance {
+    address public owner;
+
+    mapping(address => uint) public allowance;
+
+    event AllowanceChanged(address indexed _forWho, address indexed _FromWhom, uint _oldAmount, uint _newAmount);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
+    modifier Permitted(uint _amount) {
+        require(msg.sender == owner || allowance[msg.sender] >= _amount, "You are not allowed");
+        _;
+    }
+
+    function setAllowance(address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] + _amount);
+        allowance[_who] += _amount;
+    }
+
+    function reduceAllowance(address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
+        allowance[_who] -= _amount;
+    }
+}
+
+contract SharedWallet is Allowance {
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+
+    function withdrawMoney(address _to, uint _amount) public Permitted(_amount) {
+        require(_amount <= address(this).balance, "Exceeds the total contract balance");
+        if(msg.sender != owner) {
+            reduceAllowance(msg.sender, _amount);
+        }
+        emit MoneySent(_to, _amount);
+
+        (bool success, ) = _to.call{value: _amount}("");
+        require(success, "Failed to withdraw money");
+    }
+
+    receive () external payable {
+        emit MoneyReceived(msg.sender, msg.value);
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const SharedWallet = await hre.ethers.getContractFactory("SharedWallet");
+  const sharedWallet = await SharedWallet.deploy();
+
+  await sharedWallet.deployed();
+
+  console.log("SharedWallet contract deployed to:", sharedWallet.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
